5f03959fd5a66c4450c4e925ec773f61d0b283e8
Carlos N. SepúlvedaAvila <cnsa1606@gmail.com>
2018-10-04 12:08:19 -0500

    Delete SumToZero.py

--- SumToZero.py
+++
@@ -1,9 +0,0 @@
-import sys
-test_cases = open("/home/carlos/Documents/CodeEval/TestFiles/SumToZero",'r')
-for test in test_cases:
-	test = test.replace("\n","")
-	if test:
-		
-	else:
-		pass
-test_cases.close()
